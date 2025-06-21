93e52dac731c08a99d37a8f0fa8175d950d4ed16
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-06-21 18:04:08 +0500

    Created components for input and input viewing

---
+++ src/components/input.jsx
@@ -0,0 +1,39 @@
+export function ProfileInput(){
+
+    return (
+        <>
+        <div className="input"><label >Full Name</label><input type="text" ></input></div>
+        <div className="input"><label >Email</label><input type="email" ></input></div>
+        <div className="input"><label >Phone Number</label><input type="number" ></input></div>
+        <div className="input"><label >Address</label><input type="text" ></input></div>
+        </>
+    );
+}
+
+export function WorkInput(){
+
+    return (
+        <>
+        <div className="input"><label >Company Name</label><input type="text" ></input></div>
+        <div className="input"><label >Position Title</label><input type="email" ></input></div>
+        <div className="input"><label >Start Date</label><input type="date" ></input></div>
+        <div className="input"><label >End Date</label><input type="date" ></input></div>
+        <div className="input"><label >Location</label><input type="text" ></input></div>
+        <div className="input"><label >Description</label><input type="text" ></input></div>
+        </>
+    );
+}
+
+export function EducationInput(){
+
+    return (
+        <>
+        <div className="input"><label >School</label><input type="text" ></input></div>
+        <div className="input"><label >Degree</label><input type="text" ></input></div>
+        <div className="input"><label >Start Date</label><input type="date" ></input></div>
+        <div className="input"><label >End Date</label><input type="date" ></input></div>
+        <div className="input"><label >Location</label><input type="text" ></input></div>
+        <div className="input"><label >Level</label><input type="text" ></input></div>
+        </>
+    );
+}
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,13 +1,12 @@
+import { ProfileInput } from "./input";
+
 export default function Menu(){
 
     return(
         <>
             <div className="menu">
             <h1>Personal Details</h1>
-            <div className="input"><label >Full Name</label><input type="text" ></input></div>
-            <div className="input"><label >Email</label><input type="email" ></input></div>
-            <div className="input"><label >Phone Number</label><input type="number" ></input></div>
-            <div className="input"><label >Address</label><input type="text" ></input></div>
+            <ProfileInput></ProfileInput>
             </div>
         </>
     );
