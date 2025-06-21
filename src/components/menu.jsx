0a69edf041f4927651b38c7058ca65fa625b4d19
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-06-21 17:45:00 +0500

    Added basic styling

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -4,12 +4,10 @@ export default function Menu(){
         <>
             <div className="menu">
             <h1>Personal Details</h1>
-            <div className="input">
-            <label >Full Name</label><input type="text" ></input>
-            <label >Email</label><input type="email" ></input>
-            <label >Phone Number</label><input type="number" ></input>
-            <label >Address</label><input type="text" ></input>
-            </div>
+            <div className="input"><label >Full Name</label><input type="text" ></input></div>
+            <div className="input"><label >Email</label><input type="email" ></input></div>
+            <div className="input"><label >Phone Number</label><input type="number" ></input></div>
+            <div className="input"><label >Address</label><input type="text" ></input></div>
             </div>
         </>
     );
