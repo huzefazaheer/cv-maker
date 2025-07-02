41be7ba1805d1d8c3092444c4dd15a5d81b71ff8
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-02 14:39:25 +0500

    Updated cv styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { EducationInput, ProfileInput, WorkInput } from './components/input';
 
 function App() {
 
-  const [userInfo, setUserInfo] = useState({name:"John Doe", email:"mail@example.com", tel:"923101234567", location:"Islamabad. PK"});
-  const [userWork, setUserWork] = useState([{companyName:"hello", title:"world",location: "ISB, PK", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati velit animi soluta asperiores ratione maxime vero repellat ducimus perspiciatis recusandae?", startDate: "2024-07-02", endDate: "2028-07-02"}]);
-  const [userEducation, setUserEducation] = useState([{schoolName:"Nust", degree:"Electrical Engineering", edulevel:"undergrad", location:"Islamabad", startDate: "2024-07-02", endDate: "2028-07-02"}])
+  const [userInfo, setUserInfo] = useState({name:"John Doe", email:"mail@example.com", tel:"923101234567", location:"Islamabad, Pakistan"});
+  const [userWork, setUserWork] = useState([{companyName:"Umbrella Inc.", title:"Full Stack Web Dev",location: "Islamabad, Pakistan", desc: "Supported senior researchers on accessibility standards for the open  web. Created and usability tested wireframes and prototypes. Produced  interactive documentation for quick onboarding of new researchers.", startDate: "2024-07-02", endDate: "2028-07-02"}]);
+  const [userEducation, setUserEducation] = useState([{schoolName:"National University of Sciences and Technology", degree:"Bachelors in Electrical Engineering", edulevel:"Undergraduate", location:"Islamabad, Pakistan", startDate: "2024-07-02", endDate: "2028-07-02"}])
 
   return (
       <>
--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -1,11 +1,19 @@
+import "./cv.css"
+
 export default function CV({userInfo, userWork, userEducation}){
 
     return(
         <>
             <div className="cv">
-            <CVInfo userInfo={userInfo}></CVInfo>
-            <CVWork userWork={userWork}></CVWork>
-            <CVEdu userEducation={userEducation}></CVEdu>
+            <div className="cvinfo"><CVInfo userInfo={userInfo}></CVInfo></div>
+            <div className="cvedu">
+                <h2 className="cvheading">Education</h2>
+                <CVEdu userEducation={userEducation}></CVEdu>
+            </div>
+            <div className="cvwork">
+                <h2 className="cvheading">Professional Experience</h2>
+                <CVWork userWork={userWork}></CVWork>
+            </div>
             </div>
         </>
     );
@@ -15,10 +23,10 @@ function CVInfo({userInfo}){
 
     return(
         <>
-        <h2>{userInfo.name}</h2>
-            <h3>{userInfo.email}</h3>
-            <h3>{userInfo.tel}</h3>
-            <h3>{userInfo.location}</h3>
+            <h2 className="username">{userInfo.name}</h2>
+            <div className="iconholder"><img src="../../src/assets/mailicon.svg" alt="" /><h3 className="useremail">{userInfo.email}</h3></div>
+            <div className="iconholder"><img src="../../src/assets/telicon.svg" alt="" /><h3 className="usertel">{userInfo.tel}</h3></div>
+            <div className="iconholder"><img src="../../src/assets/locicon.svg" alt="" /><h3 className="userlocation">{userInfo.location}</h3></div>
             </>
     )
 }
@@ -26,14 +34,18 @@ function CVInfo({userInfo}){
 function CVWork({userWork}){
     let workDetail = []
         workDetail.push(
-            <>
-            <h2>{userWork[0].companyName}</h2>
-            <h3>{userWork[0].title}</h3>
-            <h3>{userWork[0].location}</h3>
-            <h4>{userWork[0].startDate}</h4>
-            <h4>{userWork[0].endDate}</h4>
-            <p>{userWork[0].desc}</p>
-            </>
+            <div className="cvworkdetail">
+                
+            <div className="left">
+                <div className="date"><p>{userWork[0].startDate}</p>
+            <p>{userWork[0].endDate}</p></div>
+            <p>{userWork[0].location}</p>
+            </div>
+            <div className="right">
+            <p className="bold">{userWork[0].companyName}</p>
+            <p>{userWork[0].title}</p>
+            <p className="small">{userWork[0].desc}</p></div>
+            </div>
         )
 
     return(
@@ -46,14 +58,19 @@ function CVWork({userWork}){
 function CVEdu({userEducation}){
     let workDetail = []
         workDetail.push(
-            <>
-            <h2>{userEducation[0].schoolName}</h2>
-            <h3>{userEducation[0].degree}</h3>
-            <h3>{userEducation[0].edulevel}</h3>
-            <h4>{userEducation[0].startDate}</h4>
-            <h4>{userEducation[0].endDate}</h4>
-            <h3>{userEducation[0].location}</h3>
-            </>
+            <div className="cvedudetail">
+            
+            <div className="left">
+            <div className="date"><p>{userEducation[0].startDate}</p>
+            <p>{userEducation[0].endDate}</p></div>
+            <p>{userEducation[0].location}</p>
+            </div>
+
+            <div className="right">
+            <p className="bold">{userEducation[0].schoolName}</p>
+            <p>{userEducation[0].degree}</p>
+            <p className="small">{userEducation[0].edulevel}</p></div>
+            </div>
         )
 
     return(
